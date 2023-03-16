4c69405551d9032d6d41f53c72646c13ca5d0d86
Mohamed-Helmy15 <mohamedhelmy15311@gmail.com>
2023-03-16 21:50:31 +0200

    edit some sections

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-const typeWriter = (selector) => {
-  const el = document.querySelector(selector);
-  const text = el.innerHTML;
-  (function _type(i = 0) {
-    if (i === text.length) return;
-    el.innerHTML =
-      text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
-    setTimeout(() => _type(i + 1), 100);
-  })();
-};
-typeWriter(".js-type-writer");
-
 let scrollBtn = document.querySelector(".scroll");
 let ourSkills = document.querySelector(".skills");
 window.onscroll = function () {
@@ -21,7 +9,7 @@ window.onscroll = function () {
 
   if (
     window.scrollY >= ourSkills.offsetTop - 300 &&
-    window.scrollY <= ourSkills.offsetTop + 300
+    window.scrollY <= ourSkills.offsetTop + 600
   ) {
     document
       .querySelectorAll(".skills .skills-content span")
@@ -36,3 +24,17 @@ window.onscroll = function () {
       });
   }
 };
+
+const typeWriter = (selector) => {
+  const el = document.querySelector(selector);
+  const text = el.innerHTML;
+  (function type(i = 0) {
+    if (i === text.length) return;
+    el.innerHTML =
+      text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
+    setTimeout(() => type(i + 1), 100);
+  })();
+};
+typeWriter(".js-type-writer");
+typeWriter(".h2");
+typeWriter(".span");
